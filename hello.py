a43053870dda679d2cfab3ac6e18d9e9a0c55317
Gourav Kumar <gouravin@outlook.com>
2020-08-11 15:42:47 +0530

    First commit

---
+++ hello.py
@@ -0,0 +1,66 @@
+# Normal print command
+print("Hello, World!")
+
+# sending string in var
+mystr = "Gourav"
+print(mystr)
+
+# adding something in var
+mystr += " kumar"
+print(mystr)
+
+# Changing var type
+mystr = 1
+print(mystr)
+
+# multi line string
+mystr = '''
+This is a tripe quoted
+multi-line string
+'''
+print(mystr)
+
+# Adding two string
+mystr = "pass" + "word"
+print(mystr)
+
+# Adding multiple same thing in string
+mystr = 4 * "Ha"
+print(mystr)
+mystr = "Ha" * 4
+print(mystr)
+
+# Fiding place of letter in string
+mystr = "Gourav Kumar"
+print (mystr.find('G'))
+print (mystr.find('ar'))
+
+# Chaning string into BLOCK and small
+mystr = "Gourav Kumar"
+print (mystr.lower())
+print (mystr.upper())
+
+# Play with spaces in string
+print ("Gourav\tKumar")
+print ("Gourav\nKumar")
+print ("Gourav\\nKumar")
+
+# USing quotes in strings
+print("'Gourav' Kumar")
+print('"Gourav" Kumar')
+print("\"Gourav\" Kumar")
+
+# Numeric operator
+print (2+2)          # whole
+print (2.0+2.0)      # Float
+print (4.5e9)        # Using e
+print (4.5 *(10**9)) # Using power
+print (2 + 2.0)      # will give float
+print (5/3)          # entire 
+print (5//3)         # only even numner divide
+print (5%3)          # whole number reminder
+print (6/3)          # float number
+
+
+
+
